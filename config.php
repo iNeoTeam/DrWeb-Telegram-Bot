c09a2943f064486273014bc8cc1a3e3e558b62ff
iNeoTeam | آی نئو <58592346+iNeoTeam@users.noreply.github.com>
2021-01-15 08:24:57 +0330

    bot config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(0);
+$token		= "";												// set your bot access token
+$admins		= ['0', '1'];										// set bot admins user id
+$channel	= "iNeoTeam";										// without @
+$api		= "https://api.ineo-team.ir";						// don't change it
+$cs			= ['creator', 'administrator', 'member'];			// don't change it
+$char		= array('\'', '"', '$', ';', ',', '`', '~', '*');	// don't change it
